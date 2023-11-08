6689031a29c165f168d3168e74e81abb8afe04b1
niihbotelho <nicolivictori@gmail.com>
2023-11-08 17:01:40 -0400

    contas

---
+++ aula2.js
@@ -0,0 +1,21 @@
+
+      var contador = 1
+      var soma = 0
+
+      while (contador <=5){
+        soma = soma + contador 
+        contador = contador +1
+      }
+      console.log (soma)
+      console.log ( contador)
+
+      
+
+var contador2= 5
+var fatorial= 1
+
+while (contador2 > 1){
+    fatorial = fatorial +contador2
+    contador2 = contador2 -1
+}
+console.log (fatorial)
